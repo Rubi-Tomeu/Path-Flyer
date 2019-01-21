c490d5110f77cd9d8a2313fcce209c455cf8a583
Rubi-cs2ac <46900665+Rubi-cs2ac@users.noreply.github.com>
2019-01-21 18:57:17 +0100

    Add files via upload

--- Functions/Path following/PF.m
+++ Functions/Path following/PF.m
@@ -1,4 +1,26 @@
 function [ out ] = PF( in )
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Path Following algorithm selector %
+% Tomeu Rubí                        %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Copyright (C) 2019 Bartomeu Rubí, Adrián Ruiz, Ramon Pérez, Bernardo Morcego
+%
+% Path-Flyer is free software; you can redistribute it and/or modify it under 
+% the terms of the GNU General Public License as published by the Free Software 
+% Foundation; either version 3, or (at your option) any later version.
+%
+% Path-Flyer is distributed in the hope that it will be useful, but WITHOUT ANY
+% WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+% A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along with 
+% the benchmark files. If not, see http://www.gnu.org/licenses/.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %Inputs
 Algorithm = in(13);
 Adaptive = in(14);
